59a22306e968be311c7fd2fd255863a4d69b4c60
Laurent Nunez <lnunez@hotmail.fr>
2023-02-23 18:24:45 +0100

    third section appears when click on gameButton

--- js/oneGame.js
+++ js/oneGame.js
@@ -1,18 +1,18 @@
 
+//fonction qui permet de sélectionner un match parmi la liste
+
 function selectTheGame () {
 
-const gameCardSelector = document.querySelectorAll(".gameCard");
+const gameCardSelector = document.querySelectorAll(".gameCardButton");
 
+//on lance une boucle permettant de faire apparaître la section 'third-section' en cliquant sur un des match de la liste
 for (const game of gameCardSelector) {
 
-    game.addEventListener('click', handleGameCard);
-    gameCardId = game.id;
-    //console.log(gameCardId)
-}
+    game.addEventListener('click', ()=> {
+        thirdSectionSelector.classList.remove('off');
 
-
-function handleGameCard(gameCardId) {
-    console.log(gameCardId);
-}
+    });
+    
+};
 
 }
