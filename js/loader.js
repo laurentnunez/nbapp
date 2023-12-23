f8c91074c64ed8a1b011ea86f5bafec36fc1a52e
Laurent Nunez <lnunez@hotmail.fr>
2023-12-23 17:22:21 +0100

    navbar_fixed

--- js/loader.js
+++ js/loader.js
@@ -2,6 +2,7 @@
 
     function closeLoader () {
         document.querySelector('.loader-container').classList.add('hidden');
+        document.querySelector('.menuBar').classList.remove('hidden');
 
         //handleGamesButton();
         //deleteElements();
@@ -18,13 +19,14 @@
 function showLoader () {
     function closeLoader () {
         document.querySelector('.loader-container').classList.add('hidden');
+        document.querySelector('.menuBar').classList.remove('hidden');
         
-        document.querySelector('.navbar').classList.remove('hidden');
+        
     }
 
     setTimeout(closeLoader, 1500);
 
     document.querySelector('.loader-container').classList.remove('hidden');
-    document.querySelector('.navbar').classList.add('hidden');
+    document.querySelector('.menuBar').classList.add('hidden');
 }
 
